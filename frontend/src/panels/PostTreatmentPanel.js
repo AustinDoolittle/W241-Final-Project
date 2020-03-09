c7e8d0a5c74a33e34d5b768c20feb43e90b372a8
Austin Doolittle <austindoolittle@gmail.com>
2020-03-09 18:54:38 -0400

    Remove source files for PR

--- frontend/src/panels/PostTreatmentPanel.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function PostTreatmentPanel() {
-    return <p>Here is where I'll thank you for taking part in our study and direct you to the post treatment survey</p>
-}
