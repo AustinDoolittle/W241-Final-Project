57444ae9aeef0b2c5c5db6b89319c499f119e2d6
Austin Doolittle <austindoolittle@gmail.com>
2020-03-18 10:14:50 -0400

    Fix spacing in error panels

--- frontend/src/panels/ErrorPanel.js
+++ frontend/src/panels/ErrorPanel.js
@@ -9,8 +9,8 @@ export default function ErrorPanel(props) {
         <div>
             <Typography variant='h3'>Something went wrong</Typography>
             <Typography variant='body1'>
-                {errorText} 
-                Please contact <a href={`mailto:${W241_EMAIL_ADDRESS}?Subject=Encountered%20Error`}>{W241_EMAIL_ADDRESS}}</a> for further instructions
+                {`${errorText} `} 
+                Please contact <a href={`mailto:${W241_EMAIL_ADDRESS}?Subject=Encountered%20Error`}>{W241_EMAIL_ADDRESS}</a> for further instructions
             </Typography>
         </div>
     )
