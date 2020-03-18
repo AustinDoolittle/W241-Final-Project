365ff306c6adfa8db8a2223a243f7eb4aa7fdc70
Austin Doolittle <austindoolittle@gmail.com>
2020-03-18 10:32:10 -0400

    New dummy data script for just austin

---
+++ rest_api/sql/insert_dummy_data_austin.sql
@@ -0,0 +1,26 @@
+INSERT INTO tblSubjects (
+    experiment_status,
+    assignment_status,
+    is_pilot,
+    gender,
+    email_address
+)
+VALUES (
+    'N',
+    'TM',
+    TRUE,
+    'M',
+    'austin.doolittle@berkeley.edu'
+), (
+    'N',
+    'TF',
+    TRUE,
+    'M',
+    'austin.doolittle@berkeley.edu'
+), (
+    'N',
+    'C',
+    TRUE,
+    'M',
+    'austin.doolittle@berkeley.edu'
+);
